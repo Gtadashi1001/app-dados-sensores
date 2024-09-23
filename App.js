8329c204f0a93550d491042e8479f098ab9fa8c5
gabsakura <gabsakura1@gmail.com>
2024-09-23 08:59:43 -0300

    Nova_v

--- App.js
+++ App.js
@@ -1,76 +1,20 @@
-import {
-  BarElement,
-  CategoryScale,
-  Chart as ChartJS,
-  Legend,
-  LineElement,
-  LinearScale,
-  PointElement, Title, Tooltip
-} from 'chart.js';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { StyleSheet, Text, View } from 'react-native';
+import GraphScreen from './screens/GraphScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
-// Registrando os componentes do Chart.js
-ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, PointElement, Title, Tooltip, Legend);
+const Stack = createStackNavigator();
 
 export default function App() {
-  const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-    datasets: [
-      {
-        type: 'line',
-        label: 'Vendas',
-        data: [20, 45, 28, 80, 99, 43, 50],
-        borderColor: 'rgba(75, 192, 192, 1)',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        fill: false,
-      },
-      {
-        type: 'bar',
-        label: 'Objetivos',
-        data: [30, 60, 45, 90, 70, 55, 60],
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      tooltip: {
-        callbacks: {
-          label: function (tooltipItem) {
-            return `Valor: ${tooltipItem.raw}`;
-          },
-        },
-      },
-    },
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Gráfico Combinado</Text>
-      <Line data={data} options={options} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="GraphScreen" component={GraphScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
